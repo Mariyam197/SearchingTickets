fd0daa0c20e6eea4bbf7f833f30af3f9b53d87ad
Mariam Kazaryan <mariyam.k.077@gmail.com>
2023-01-22 14:47:59 +0300

    add changes

--- src/main/java/ru/netology/manager/TicketManager.java
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -35,22 +35,6 @@ public class TicketManager {
     }
 
 
-    public Ticket[] findAll(String from, String to, Comparator<Ticket> comparator) {
-        Ticket[] result = new Ticket[0];
-        for (Ticket ticket : repository.findAll()) {
-            if (matches(ticket, from, to)) {
-                Ticket[] tmp = new Ticket[result.length + 1];
-                for (int i = 0; i < result.length; i++) {
-                    tmp[i] = result[i];
-                }
-                tmp[result.length] = ticket;
-                result = tmp;
-            }
-        }
-        Arrays.sort(result, comparator);
-        return result;
-    }
-
     private boolean matches(Ticket ticket, String from, String to) {
         if (ticket.getFrom().equals(from)) {
             if (ticket.getTo().equals(to)) {
--- src/test/java/ru/netology/manager/TicketManagerTest.java
+++ src/test/java/ru/netology/manager/TicketManagerTest.java
@@ -3,22 +3,20 @@ package ru.netology.manager;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import ru.netology.comparator.TicketByTimeComparator;
 import ru.netology.domain.Ticket;
 import ru.netology.repository.TicketRepository;
 
 public class TicketManagerTest {
     TicketRepository repository = new TicketRepository();
     TicketManager manager = new TicketManager(repository);
-    TicketByTimeComparator comparator = new TicketByTimeComparator();
 
     Ticket ticket1 = new Ticket(1, 200, "MSK", "SPB", 160);
     Ticket ticket2 = new Ticket(2, 230, "SPB", "MSK", 160);
     Ticket ticket3 = new Ticket(3, 360, "MSK", "SMR", 100);
-    Ticket ticket4 = new Ticket(4, 330, "MSK", "SPB", 150);
-    Ticket ticket5 = new Ticket(5, 500, "MSK", "SPB", 180);
+    Ticket ticket4 = new Ticket(4, 330, "MSK", "SPB", 160);
+    Ticket ticket5 = new Ticket(5, 500, "MSK", "SPB", 160);
     Ticket ticket6 = new Ticket(6, 390, "LA", "NY", 180);
-    Ticket ticket7 = new Ticket(7, 220, "MSK", "SPB", 100);
+    Ticket ticket7 = new Ticket(7, 220, "MSK", "SPB", 160);
     Ticket ticket8 = new Ticket(8, 420, "MSK", "SPB", 160);
 
 
@@ -71,23 +69,6 @@ public class TicketManagerTest {
 
         Ticket[] expected = {};
         Ticket[] actual = manager.findAll("SMR", "MSK");
-
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldSearchTheFastest() {
-        manager.add(ticket1);
-        manager.add(ticket2);
-        manager.add(ticket3);
-        manager.add(ticket4);
-        manager.add(ticket5);
-        manager.add(ticket6);
-        manager.add(ticket7);
-        manager.add(ticket8);
-
-        Ticket[] expected = {ticket7, ticket4, ticket1, ticket8, ticket5};
-        Ticket[] actual = manager.findAll("MSK", "SPB",comparator);
 
         Assertions.assertArrayEquals(expected, actual);
     }
